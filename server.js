2e9a1a3267278f20ded708a6c7ee802a3d1b1697
Gabriel Morris <gabemorris@outlook.com>
2018-09-04 09:10:36 -0700

    Added both GET endpoints

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+
+// Static server
+app.use(express.static('public'));
+
+// Listen
+app
+  .listen(8080, function() {
+    console.log(`Server listening on ${this.address().port}`);
+  })
+  .on('error', err => {
+    console.error(err);
+  });
+
+// Get all notes
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+// Get a note by ID
+app.get('/api/notes/:id', (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id;
+  console.log(id);
+  const foundItem = data.find(item => item.id === Number(id));
+  res.json(foundItem);
+});
