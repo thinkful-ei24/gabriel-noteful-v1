ef38ff4e770416d8fd526a04706f9832b8a2cab6
Gabriel Morris <gabemorris@outlook.com>
2018-09-04 09:29:43 -0700

    Added search functionality

--- server.js
+++ server.js
@@ -23,14 +23,46 @@ app
 
 // Get all notes
 app.get('/api/notes', (req, res) => {
-  res.json(data);
+  // Get the search term
+  const { searchTerm } = req.query;
+  // Send the result of a ternary
+  res.json(
+    checkIfRequestHasSearch(searchTerm)
+      ? buildFilteredArrayOfItems(searchTerm)
+      : data
+  );
 });
 
 // Get a note by ID
 app.get('/api/notes/:id', (req, res) => {
-  console.log(req.params.id);
-  const id = req.params.id;
-  console.log(id);
-  const foundItem = data.find(item => item.id === Number(id));
-  res.json(foundItem);
+  res.json(findItemByID(getItemID(req)));
 });
+
+// Function for checking if we have the search term
+function checkIfRequestHasSearch(searchTerm) {
+  if (searchTerm) return true;
+  return false;
+}
+
+// Function for building a filtered array of items
+function buildFilteredArrayOfItems(searchTerm) {
+  return data.filter(item => {
+    if (checkIfItemHasSearchTerm(searchTerm, item)) return item;
+  });
+}
+
+// Function for checking if item includes search term
+function checkIfItemHasSearchTerm(searchTerm, item) {
+  if (item.title.includes(searchTerm) || item.content.includes(searchTerm))
+    return item;
+}
+
+// Function for getting the id from a request
+function getItemID(request) {
+  return request.params.id;
+}
+
+// Function for finding an item
+function findItemByID(id) {
+  return data.find(item => item.id === Number(id));
+}
