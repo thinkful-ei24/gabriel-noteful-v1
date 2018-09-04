692f7fde6f438e6a8136bfa1bca7f8ba0913ceb6
Gabriel Morris <gabemorris@outlook.com>
2018-09-04 15:05:30 -0700

    Refactored and extracted functions

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-// Load simDB and Express
+/***** Variables and modules *****/
 const data = require('./db/notes');
 const simDB = require('./db/simDB');
 const notes = simDB.initialize(data);
@@ -11,43 +11,42 @@ const { logErrors } = require('./middleware/logger');
 // Static server
 app.use(express.static('public'));
 
+// Add body parser
+app.use(express.json());
+
+/***** HTTP request handlers *****/
 // Get all notes
 app.get('/api/notes', (req, res, next) => {
-  // Get the search term
-  const { searchTerm } = req.query;
-  // Send the result of a ternary
-  notes.filter(searchTerm, (err, list) => {
-    if (err) return next(err);
-    res.json(list);
-  });
+  getAllNotes(req, res, next);
 });
+
+// Function for returning the search term from a query object
+function destructureSearchTerm(req) {
+  return req.query.searchTerm;
+}
 
 // Get a note by ID
 app.get('/api/notes/:id', (req, res, next) => {
-  notes.find(getItemID(req), (err, item) => {
-    if (err) return next(err);
-    res.json(item);
-  });
+  findNoteByID(req, res, next);
 });
 
-// Error logging
+// Update a note by ID
+app.put('/api/notes/:id', (req, res, next) => {
+  updateNoteByID(req, res, next);
+});
+
+/***** Error logging *****/
 app.use(logErrors);
 
 // 404 not found handler
 app.use(function(req, res, next) {
   const err = new Error('Not found');
-  err.status = 404;
-  res.status(404).json({ message: 'Not found' });
-  next(err);
+  handle404Error(res, err, next);
 });
 
 // Catch all error handler
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
+  handleCatchAllErrors(res, err);
 });
 
 // Listen
@@ -60,32 +59,82 @@ app
   });
 
 /***** Functions *****/
-// Function for checking if we have the search term
-function checkIfRequestHasSearch(searchTerm) {
-  if (searchTerm) return true;
-  return false;
-}
 
-// Function for building a filtered array of items
-function buildFilteredArrayOfItems(searchTerm) {
-  return data.filter(item => {
-    if (checkIfItemHasSearchTerm(searchTerm, item)) return item;
+/*** Routing functions ***/
+// Function for getting all notes
+function getAllNotes(req, res, next) {
+  // Get the search term
+  const searchTerm = destructureSearchTerm(req);
+  // Send the result of a ternary
+  notes.filter(searchTerm, (err, list) => {
+    generateResponse(err, list, next, res);
   });
 }
 
-// Function for checking if item includes search term
-function checkIfItemHasSearchTerm(searchTerm, item) {
-  if (item.title.includes(searchTerm) || item.content.includes(searchTerm))
-    return item;
+// Function for updating a note by ID
+function updateNoteByID(req, res, next) {
+  // Validate input
+  const { id, updateObject, updateFields } = createValidationObject(req);
+  validateInput(updateObject, updateFields, req);
+
+  notes.update(id, updateObject, (err, item) => {
+    generateResponse(err, item, next, res);
+  });
 }
 
+// Function for finding note by ID
+function findNoteByID(req, res, next) {
+  notes.find(getItemID(req), (err, item) => {
+    generateResponse(err, item, next, res);
+  });
+}
+
+/*** Error handler functions ***/
+function handle404Error(res, err, next) {
+  err.status = 404;
+  res.status(404).json({ message: 'Not found' });
+  next(err);
+}
+function handleCatchAllErrors(res, err) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+}
+
+/*** Utility functions ***/
 // Function for getting the id from a request
 function getItemID(request) {
-  console.log(request.params.id);
   return request.params.id;
 }
 
-// Function for finding an item
-function findItemByID(id) {
-  return data.find(item => item.id === Number(id));
+// Function for validating input
+function validateInput(updateObject, updateFields, req) {
+  // Look through all fields and check if the field is in the request body
+  updateFields.forEach(field => {
+    if (field in req.body) updateObject[field] = req.body[field];
+  });
+}
+
+// Function for updating item
+function generateResponse(err, item, next, res) {
+  // If there's an error call the next error middleware
+  if (err) return next(err);
+  // If item exists respond with it, otherwise go to the next
+  item ? res.json(item) : next();
+}
+
+// Function for creating update object and fields
+function createValidationObject(req) {
+  return {
+    id: createIDObject(req),
+    updateObject: {},
+    updateFields: ['title', 'content']
+  };
+}
+
+// Function for creating ID object
+function createIDObject(req) {
+  return getItemID(req);
 }
