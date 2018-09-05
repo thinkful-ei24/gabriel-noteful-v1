9415454036794f1d786182344be7bc97c0d12334
Gabriel Morris <gabemorris@outlook.com>
2018-09-05 13:41:25 -0700

    Finished working on all CRUD functionality

--- router/notes.router.js
+++ router/notes.router.js
@@ -9,29 +9,44 @@ const notes = simDB.initialize(data);
 const { truthy, buildNewError } = require('../lib/utilities');
 
 /***** HTTP request handlers *****/
-// Create new note
+/*** Create new note ***/
 router.post('/notes', (req, res, next) => {
-  createNote(req, res, next);
+  createNoteHandler(req, res, next);
 });
 
-// GET all notes
+/*** GET all notes ***/
 router.get('/notes', (req, res, next) => {
-  getAllNotes(req, res, next);
+  getAllNoteHandler(req, res, next);
 });
 
-// GET note ID
+/*** GET note ID ***/
 router.get('/notes/:id', (req, res, next) => {
-  findNoteByID(req, res, next);
+  getNoteByID(req, res, next);
 });
 
-// Update note by ID
+/*** Update note by ID ***/
 router.put('/notes/:id', (req, res, next) => {
-  updateNoteByID(req, res, next);
+  handleUpdateNoteByID(req, res, next);
+});
+
+/*** Delete note by ID ***/
+router.delete('/notes/:id', (req, res, next) => {
+  if (truthy(getItemID(req))) deleteNoteByID(getItemID(req), req, res, next);
 });
 
 /***** Routing functions *****/
-// Function for getting all notes
-function getAllNotes(req, res, next) {
+
+/*** CRUD handlers ***/
+// CREATE
+function createNoteHandler(req, res, next) {
+  // Create and validate new item
+  const newItem = buildNewItemObject(req.body);
+  validateNewItem(newItem, next);
+  addNoteToDB(req, res, next, newItem);
+}
+
+// READ
+function getAllNoteHandler(req, res, next) {
   // Get the search term
   const searchTerm = destructureSearchTerm(req);
   // Send the result of a ternary
@@ -40,8 +55,8 @@ function getAllNotes(req, res, next) {
   });
 }
 
-// Function for updating a note by ID
-function updateNoteByID(req, res, next) {
+// UPDATE
+function handleUpdateNoteByID(req, res, next) {
   // Validate input
   const { id, updateObject, updateFields } = createValidationObject(req);
   validateUpdateInput(updateObject, updateFields, req);
@@ -51,22 +66,28 @@ function updateNoteByID(req, res, next) {
   });
 }
 
+// DELETE
+function deleteNoteByID(id, req, res, next) {
+  notes.delete(id, (err, item) => {
+    if (err) return next(err);
+
+    if (item) {
+      res.status(204).send();
+    } else {
+      err.status = 500;
+      next();
+    }
+  });
+}
+
 // Function for finding note by ID
-function findNoteByID(req, res, next) {
+function getNoteByID(req, res, next) {
   notes.find(getItemID(req), (err, item) => {
     generateResponse(err, item, next, res);
   });
 }
 
-// Function for creating a new note
-function createNote(req, res, next) {
-  // Create and validate new item
-  const newItem = buildNewItemObject(req.body);
-  validateNewItem(newItem, next);
-  addNoteToDB(req, res, next, newItem);
-}
-
-/*** Utility functions ***/
+/***** Utility functions *****/
 // Function for getting the id from a request
 function getItemID(request) {
   return request.params.id;
@@ -77,19 +98,6 @@ function createIDObject(req) {
   return getItemID(req);
 }
 
-// Function for returning the search term from a query object
-function destructureSearchTerm(req) {
-  return req.query.searchTerm;
-}
-
-// Function for validating input
-function validateUpdateInput(updateObject, updateFields, req) {
-  // Look through all fields and check if the field is in the request body
-  updateFields.forEach(field => {
-    if (field in req.body) updateObject[field] = req.body[field];
-  });
-}
-
 // Function for updating item
 function generateResponse(err, item, next, res) {
   // If there's an error call the next error middleware
@@ -98,15 +106,9 @@ function generateResponse(err, item, next, res) {
   item ? res.json(item) : next();
 }
 
-// Function for creating update object and fields
-function createValidationObject(req) {
-  return {
-    id: createIDObject(req),
-    updateObject: {},
-    updateFields: ['title', 'content']
-  };
-}
+/*** CRUD utilities ***/
 
+/* CREATE utilities */
 // Function for body request and building new item obj
 function buildNewItemObject(req) {
   return { title: req.title, content: req.content };
@@ -114,6 +116,7 @@ function buildNewItemObject(req) {
 
 // Function for new item validation
 function validateNewItem(object, next) {
+  // Check if title is not truthy and return an error
   if (!truthy(object.title)) {
     const err = buildNewError('Missing title in request body');
     err.status = 400;
@@ -123,9 +126,11 @@ function validateNewItem(object, next) {
 
 // Function for creating new note
 function addNoteToDB(req, res, next, note) {
+  // Call notes.create
   notes.create(note, (err, item) => {
     if (err) return next(err);
 
+    // Respond with location and status 201
     if (item) {
       res
         .location(`http://${req.headers.host}/notes/${item.id}`)
@@ -134,6 +139,30 @@ function addNoteToDB(req, res, next, note) {
     } else {
       next();
     }
+  });
+}
+
+/* READ utilities */
+// Function for returning the search term from a query object
+function destructureSearchTerm(req) {
+  return req.query.searchTerm;
+}
+
+/* UPDATE utilities */
+// Function for creating update object and fields
+function createValidationObject(req) {
+  return {
+    id: createIDObject(req),
+    updateObject: {},
+    updateFields: ['title', 'content']
+  };
+}
+
+// Function for validating input
+function validateUpdateInput(updateObject, updateFields, req) {
+  // Look through all fields and check if the field is in the request body
+  updateFields.forEach(field => {
+    if (field in req.body) updateObject[field] = req.body[field];
   });
 }
 
